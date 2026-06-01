a19e46b2dc70b3e0f4e6fe3ca049ee7c293bc804
dushyantzz <157586156+dushyantzz@users.noreply.github.com>
2026-06-01 15:01:07 +0530

    feat: add logistic regression hyperparameter tuning script with MLflow integration

--- notebooks/exp3_lor_bow_hp.py
+++ notebooks/exp3_lor_bow_hp.py
@@ -21,8 +21,8 @@ warnings.filterwarnings("ignore")
 # os.environ["MLFLOW_DISABLE_ARTIFACTS_DOWNLOAD"] = "1"
 
 # Set MLflow Tracking URI & DAGsHub integration
-MLFLOW_TRACKING_URI = "https://dagshub.com/vikashdas770/YT-Capstone-Project.mlflow"
-dagshub.init(repo_owner="vikashdas770", repo_name="YT-Capstone-Project", mlflow=True)
+MLFLOW_TRACKING_URI = "https://dagshub.com/dushyantkv508/MLOps-Capstone-Project.mlflow"
+dagshub.init(repo_owner="dushyantkv508", repo_name="MLOps-Capstone-Project", mlflow=True)
 mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
 mlflow.set_experiment("LoR Hyperparameter Tuning")
 
